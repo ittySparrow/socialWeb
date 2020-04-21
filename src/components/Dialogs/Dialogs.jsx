d409f670752eb599bb8da65114f54e31d8f6bfe6
anemoi <tds.index@gmail.com>
2020-04-21 16:09:52 +1000

    lesson 21 dialogs-style

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import style from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog + " " + style.active}>
+                    <NavLink to="/dialogs/1">Dimych</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/2">Sergei</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/3">Andrey</NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>
+                    Don't give up.
+                </div>
+                <div className={style.message}>
+                    You can do it, I promise.
+                </div>
+                <div className={style.message}>
+                    Let's do it, girl.
+                </div>
+
+            </div>
         </div>
     )
 }
