bdc54f1fa1034fded5418ab34ebc49d6b19c568d
ittySparrow <ittysparrow@gmail.com>
2020-06-10 15:44:25 +1000

    lesson 56

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 import { connect } from "react-redux";
-import UsersAPIComponent from "./UsersAPIComponent";
 import { toggleFollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/usersReducer';
+import * as axios from 'axios';
+import Users from './Users';
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then(({ data }) => {
+            this.props.setUsers(data.items);
+            this.props.setTotalUsersCount(data.totalCount);
+        });   
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        .then(({ data }) => this.props.setUsers(data.items));   
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      users={this.props.users}
+                      onPageChanged={this.onPageChanged}
+                      toggleFollow={this.props.toggleFollow}/>
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -21,4 +48,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
