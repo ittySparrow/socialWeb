12036072e5febff2fbcf0f449ddafad4e6bbbfd4
ittySparrow <ittysparrow@gmail.com>
2020-06-16 13:29:13 +1000

    prettify

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import {addNewMessage, handleMessageChange} from "../../redux/dialogsReducer";
+import React from "react";
+import { addNewMessage, handleMessageChange } from "../../redux/dialogsReducer";
 import Dialogs from "./Dialogs";
-import { connect } from 'react-redux';
-import withAuthRedirect from '../HOC/withAuthRedirect';
-import { compose } from 'redux';
+import { connect } from "react-redux";
+import withAuthRedirect from "../HOC/withAuthRedirect";
+import { compose } from "redux";
 
 const mapStateToProps = (state) => {
-    return {
-        newMessageText: state.messagesPage.newMessageText,
-        dialogsData: state.messagesPage.dialogsData,
-        messagesData: state.messagesPage.messagesData,
-    }
+  return {
+    newMessageText: state.messagesPage.newMessageText,
+    dialogsData: state.messagesPage.dialogsData,
+    messagesData: state.messagesPage.messagesData,
+  };
 };
 
 const mapDispatchToProps = {
-    handleMessageChange,
-    addNewMessage,
-}
+  handleMessageChange,
+  addNewMessage,
+};
 
 export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    withAuthRedirect
+  connect(mapStateToProps, mapDispatchToProps),
+  withAuthRedirect
 )(Dialogs);
