14cb16def701725e082a2f1c5b89158763dac775
ittySparrow <ittysparrow@gmail.com>
2020-07-28 12:28:11 +1000

    lesson 91 redux extention for chrome

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import sidebarReducer from "./sidebarReducer";
@@ -15,9 +15,10 @@ const reducers = combineReducers({
   auth: authReducer,
   app: appReducer,
 });
-
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
-
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
