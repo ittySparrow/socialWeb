719c7b3f6ca176ca561325df844339e7c61b2a6d
ittySparrow <ittysparrow@gmail.com>
2020-07-29 20:33:34 +1000

    gh-pages deploy l95

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 import store from "./redux/reduxStore";
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
