3216c7739e7f4394a503c8224f034dd80291432e
ittySparrow <ittysparrow@gmail.com>
2020-07-29 18:28:58 +1000

    react.lazy, suspense l94

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import { Route } from "react-router-dom";
 import { initializeApp } from "./redux/appReducer";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import { connect } from "react-redux";
 import Preloader from "./components/common/Preloader";
+import { Suspense } from "react";
+
+const UsersContainer = React.lazy(() =>
+  import("./components/Users/UsersContainer")
+);
 
 class App extends React.Component {
   componentDidMount() {
@@ -29,7 +33,14 @@ class App extends React.Component {
         <div className="app-wrapper-content">
           <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
           <Route path="/dialogs" render={() => <DialogsContainer />} />
-          <Route path="/users" component={() => <UsersContainer />} />
+          <Route
+            path="/users"
+            component={() => (
+              <Suspense fallback={<Preloader />}>
+                <UsersContainer />
+              </Suspense>
+            )}
+          />
           <Route path="/login" component={() => <Login />} />
         </div>
       </div>
